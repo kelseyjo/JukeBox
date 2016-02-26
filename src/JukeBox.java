ca734115dd1aa9e17e9ed1bbdde36c0ca38e7c6c
Love4653 <Kaiya.park@gmail.com>
2016-02-25 16:57:44 -0800

    y

---
+++ src/JukeBox.java
@@ -0,0 +1,212 @@
+// Copyright The League of Amazing Programmers, 2015
+import java.awt.Button;
+import java.awt.Label;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.advanced.AdvancedPlayer;
+
+/* 1. Download the JavaZoom jar from here: http://bit.ly/javazoom
+ * 2. Right click your project and add it as an External JAR (Under Java Build Path > Libraries).*/
+
+public class JukeBox implements MouseListener {
+
+	private JLabel label1;
+	private JLabel label2;
+	private JLabel label3;
+	private Song song = null;
+	
+	public static void main(String[] args) throws IOException,
+			JavaLayerException {
+		// 3. Find an mp3 on your computer or on the Internet.
+		// 4. Create a Song
+		
+		// 5. Play the Song
+		
+		new JukeBox().run();
+		
+		/*
+		 * 6. Create a user interface for your Jukebox so that the user can to
+		 * choose which song to play. You can use can use a different button for
+		 * each song, or a picture of the album cover. When the button or album
+		 * cover is clicked, stop the currently playing song, and play the one
+		 * that was selected.
+		 */
+		
+	}
+
+	private void run() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		label1 = loadImage("AdelePic.png");
+		label1.addMouseListener(this);
+		label2 = loadImage("DrakePic.png");
+		label2.addMouseListener(this);
+		label3 = loadImage("random.png");
+		label3.addMouseListener(this);
+		panel.add(label1);
+		panel.add(label2);
+		panel.add(label3);
+		frame.add(panel);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+	/* Use this method to add album covers to your Panel. */
+	private JLabel loadImage(String fileName) {
+		URL imageURL = getClass().getResource(fileName);
+		Icon icon = new ImageIcon(imageURL);
+		return new JLabel(icon);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		Song hotline = new Song("Drake - Hotline Bling.mp3");
+		Song hello = new Song("Adele - Hello.mp3"); 
+		Song random = new Song("Sad Violin Melody - Air Horn FULL.mp3");
+		
+		if(e.getSource() == label1)
+		{
+			if(song != null)
+				song.stop();
+			song = new Song("Adele - Hello.mp3");
+		}
+		else if(e.getSource() == label2)
+		{
+			if(song != null)
+				song.stop();
+			song = new Song("Drake - Hotline Bling.mp3");
+		}
+		else if(e.getSource() == label3)
+		{
+			if(song != null)
+				song.stop();
+			song = new Song("Sad Violin Melody - Air Horn FULL.mp3");
+		}
+		song.play();
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+class Song {
+
+	private int duration;
+	private String songAddress;
+	private AdvancedPlayer mp3Player;
+	private InputStream songStream;
+
+	/**
+	 * Songs can be constructed from files on your computer or Internet
+	 * addresses.
+	 * 
+	 * Examples: <code> 
+	 * 		new Song("Adele - Hello.mp3"); 	//from default package 
+	 * 		new Song("/Users/joonspoon/music/Vampire Weekend - Modern Vampires of the City/03 Step.mp3"); 
+	 * 		new	Song("http://freedownloads.last.fm/download/569264057/Get%2BGot.mp3"); 
+	 * </code>
+	 */
+	public Song(String songAddress) {
+		this.songAddress = songAddress;
+	}
+
+	public void play() {
+		loadFile();
+		if (songStream != null) {
+			loadPlayer();
+			startSong();
+		} else
+			System.err.println("Unable to load file: " + songAddress);
+	}
+
+	public void setDuration(int seconds) {
+		this.duration = seconds * 100;
+	}
+
+	public void stop() {
+		if (mp3Player != null)
+			mp3Player.close();
+	}
+
+	private void startSong() {
+		Thread t = new Thread() {
+			public void run() {
+				try {
+					if (duration > 0)
+						mp3Player.play(duration);
+					else
+						mp3Player.play();
+				} catch (Exception e) {
+				}
+			}
+		};
+		t.start();
+	}
+
+	private void loadPlayer() {
+		try {
+			this.mp3Player = new AdvancedPlayer(songStream);
+		} catch (Exception e) {
+		}
+	}
+
+	private void loadFile() {
+		if (songAddress.contains("http"))
+			this.songStream = loadStreamFromInternet();
+		else
+			this.songStream = loadStreamFromComputer();
+	}
+
+	private InputStream loadStreamFromInternet() {
+		try {
+			return new URL(songAddress).openStream();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	private InputStream loadStreamFromComputer() {
+		try {
+			return new FileInputStream(songAddress);
+		} catch (FileNotFoundException e) {
+			return this.getClass().getResourceAsStream(songAddress);
+		}
+	}
+}
